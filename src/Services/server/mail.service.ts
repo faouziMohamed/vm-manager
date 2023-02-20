73cc1bbea82b5beb1c5d0318d0e770cfbb2a7c18
Faouzi Mohamed <hello@mfaouzi.com>
2023-02-20 12:44:46 +0000

    fix: configure nodemailer to use secure connexion to smtp server

--- src/Services/server/mail.service.ts
+++ src/Services/server/mail.service.ts
@@ -1,40 +1,73 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 // ttyuid@protonmail.com
-import { createTransport } from 'nodemailer';
+import nodemailer from 'nodemailer';
 
 import { createVerificationToken } from '@/lib/db/queries';
 import { jwtSignData } from '@/lib/server.utils';
 import { AppUser } from '@/lib/types';
+import { AuthError } from '@/lib/utils';
 
-const transporter = createTransport(process.env.EMAIL_SERVER);
+const host = process.env.EMAIL_SERVER_HOST!;
+const port = Number(process.env.EMAIL_SERVER_PORT!);
+const user = process.env.EMAIL_SERVER_USER!;
+const pass = process.env.EMAIL_SERVER_PASSWORD!;
+const secure = true;
+
+const transporter = nodemailer.createTransport({
+  port,
+  host,
+  secure,
+  auth: { user, pass },
+  connectionTimeout: 25000,
+});
 
 async function sendVerificationLink(user: AppUser, token: string) {
-  // eslint-disable-next-line no-console
-  console.log('Sending email to', user.email);
-  const link = `${process.env.NEXTAUTH_URL}/api/auth/verify-email?token=${token}`;
-  return transporter.sendMail({
-    from: process.env.EMAIL_FROM,
-    to: user.email,
-    subject: 'Verify your email address',
-    text: `Click on the following link to verify your email address: ${link}`,
-    html: ` <p>Click on the following link to verify your email address:</p>
-            <p>
-              <a href="${link}">${link}</a>
-            </p>`,
-  });
+  try {
+    // eslint-disable-next-line no-console
+    console.log(
+      'Sending email to',
+      user.email,
+      'using ',
+      process.env.EMAIL_FROM,
+    );
+    const link = `${process.env.NEXTAUTH_URL}/api/auth/verify-email?token=${token}`;
+    return transporter.sendMail(
+      {
+        from: process.env.EMAIL_FROM,
+
+        to: user.email,
+        subject: 'Verify your email address',
+        text: `Click on the following link to verify your email address: ${link}`,
+        html: ` <p>Click on the following link to verify your email address:</p>
+              <p>
+                <a href="${link}">${link}</a>
+              </p>`,
+      },
+      // (err, info) => {
+      //   console.log('Call back function');
+      //   console.log(err, info);
+      //   console.log('Call back function end');
+      // },
+    );
+  } catch (error) {
+    console.log('Error sending email');
+    console.log(error);
+  }
 }
 
 export async function sendVerificationEmail(user: AppUser) {
-  const tk = jwtSignData({ email: user.email });
-  await createVerificationToken(user.id, tk);
   try {
+    const tk = jwtSignData({ email: user.email });
+    await createVerificationToken(user.id, tk);
     const ack = await sendVerificationLink(user, tk);
     // eslint-disable-next-line no-console
-    console.log('Email sent', ack.messageId);
+    console.log('Email sent', ack);
   } catch (err) {
     const e = err as Error;
     // eslint-disable-next-line no-console
-    console.error('Error sending email', e.message);
+    console.error('Error sending email');
+    console.dir( e, { depth: null });
+    throw new AuthError(e.message);
   }
 }
 
